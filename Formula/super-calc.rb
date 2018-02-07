afb096a337cb0e6b572bc15ec0f150fa3e2eb6fa
Charlton Trezevant <ct@ctis.me>
2018-02-06 20:24:36 -0500

    Added initial version of SuperCalc formula

---
+++ Formula/super-calc.rb
@@ -0,0 +1,17 @@
+class SuperCalc < Formula
+  desc "SuperCalc is a mathematical expression parser & evaluator, plus more"
+  homepage "https://github.com/ctrezevant/SuperCalc"
+  url "https://github.com/ctrezevant/SuperCalc.git"
+  version "1.0.0"
+
+  def install
+    system "./autogen.sh"
+    system "./configure"
+    system "make"
+    bin.install "sc"
+  end
+
+  test do
+    system "#{bin}/sc"
+  end
+end
