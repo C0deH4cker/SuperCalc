c49f3ebcf408efc06e1f0c5895a199b891413b46
Charlton Trezevant <ct@ctis.me>
2018-02-06 20:46:47 -0500

    Update formula to reflect SuperCalc repo appropriately after merge

--- Formula/super-calc.rb
+++ Formula/super-calc.rb
@@ -1,10 +1,10 @@
 class SuperCalc < Formula
   desc "SuperCalc is a mathematical expression parser & evaluator, plus more"
-  homepage "https://github.com/ctrezevant/SuperCalc"
-  url "https://github.com/ctrezevant/SuperCalc.git"
+  homepage "https://github.com/C0deH4cker/SuperCalc"
+  url "https://github.com/C0deH4cker/SuperCalc.git"
   version "1.0.0"
  
-  # Needed to override Brew's Superenv
+  # Needed to override Brew's Superenv, which was breaking our build
   env :std
 
   def install
